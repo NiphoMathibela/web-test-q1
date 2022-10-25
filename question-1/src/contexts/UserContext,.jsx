687e2430a63874477a63bbe346afb30bf582b7f8
Nhlakanipho Mathibela <94120861+NiphoMathibela@users.noreply.github.com>
2022-10-25 18:01:00 +0200

    Set up

--- question-1/src/contexts/UserContext,.jsx
+++ question-1/src/contexts/UserContext,.jsx
@@ -1,44 +1,44 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 export const UserContext = createContext();
 
-const [cardNum, setCardNum] = useState("################");
-const [cardHolder, setCardHolder] = useState("");
-const [month, setMonth] = useState("");
-const [cvv, setCvv] = useState();
-const [cardFlip, setCardFlip] = useState(false);
-const [message, setMessage] = useState("");
-
-const expSplit = month.split("-");
-const expMonth = expSplit[1];
-const expYear = expSplit[0];
-
-const handleCardNum = (e) => {
-  setCardNum(e.target.value);
-  console.log(cardNum);
-};
-
-const handleCardHolder = (e) => {
-  setCardHolder(e.target.value);
-  console.log(cardHolder);
-};
-
-const handleMonth = (e) => {
-  setMonth(e.target.value);
-  console.log(month);
-};
-
-const handleCvv = (e) => {
-  setCvv(e.target.value);
-  setCardFlip(false);
-  console.log(cvv);
-};
-
-const flip = () => {
-  setCardFlip((prev) => !prev);
-};
-
 const UserContextProvider = (props) => {
+  const [cardNum, setCardNum] = useState("################");
+  const [cardHolder, setCardHolder] = useState("");
+  const [month, setMonth] = useState("");
+  const [cvv, setCvv] = useState();
+  const [cardFlip, setCardFlip] = useState(false);
+  const [message, setMessage] = useState("");
+
+  const expSplit = month.split("-");
+  const expMonth = expSplit[1];
+  const expYear = expSplit[0];
+
+  const handleCardNum = (e) => {
+    setCardNum(e.target.value);
+    console.log(cardNum);
+  };
+
+  const handleCardHolder = (e) => {
+    setCardHolder(e.target.value);
+    console.log(cardHolder);
+  };
+
+  const handleMonth = (e) => {
+    setMonth(e.target.value);
+    console.log(month);
+  };
+
+  const handleCvv = (e) => {
+    setCvv(e.target.value);
+    setCardFlip(false);
+    console.log(cvv);
+  };
+
+  const flip = () => {
+    setCardFlip((prev) => !prev);
+  };
+
   return (
     <UserContext.Provider
       value={{
--- question-1/src/pages/Login.jsx
+++ question-1/src/pages/Login.jsx
@@ -10,11 +10,27 @@ import {
   IonIcon,
 } from "@ionic/react";
 import ExploreContainer from "../components/ExploreContainer";
-import { cameraOutline } from 'ionicons/icons';
+import { cameraOutline } from "ionicons/icons";
 import "./Login.css";
 import Card from "../components/Card";
+import { useContext } from "react";
+import { UserContext } from "../contexts/UserContext,";
 
 const Login = () => {
+  const {
+    cardNum,
+    setCardNum,
+    cardHolder,
+    setCardHolder,
+    month,
+    setMonth,
+    cvv,
+    setCvv,
+    cardFlip,
+    setCardFlip,
+    message,
+    setMessage,
+  } = useContext(UserContext);
   return (
     <IonPage>
       <IonHeader>
@@ -31,7 +47,9 @@ const Login = () => {
         <IonCard className="ion-padding form">
           <div className="img-container">
             <img className="user-img" src="" />
-            <div className="icon-box"><IonIcon icon = {cameraOutline} size= "large"/></div>
+            <div className="icon-box">
+              <IonIcon icon={cameraOutline} size="large" />
+            </div>
           </div>
 
           <IonItem>
@@ -47,9 +65,8 @@ const Login = () => {
             <IonInput type="text" placeholder="Phone number"></IonInput>
           </IonItem>
 
-
           <div>
-            <Card/>
+            <Card />
           </div>
         </IonCard>
       </IonContent>
